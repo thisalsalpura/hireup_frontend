9ad3c9aaaae26170ae5dd9352cd3db8545241e2a
thisalsalpura <salpurathisal@gmail.com>
2025-04-24 23:54:21 +0530

    Update 1.9

--- src/pages/SingleGigView.jsx
+++ src/pages/SingleGigView.jsx
@@ -46,7 +46,7 @@ const SingleGigView = () => {
                 </div>
 
                 <div className="grid grid-cols-12 mt-8">
-                    <div className="col-span-7 h-auto flex flex-col justify-center pr-5">
+                    <div className="col-span-7 h-full flex flex-col justify-center pr-5">
                         <p className="text-3xl text-black text-left">develop custom web applications business e commerce and landing pages</p>
 
                         <div className="flex flex-row items-center justify-start gap-3.5 mt-8">
@@ -134,9 +134,117 @@ const SingleGigView = () => {
 
                             <p className="mt-3.5 text-black">SMCSE was wonderful to work with! They understood my business needs and expectations and went above and beyond with their deliverables. I will hire them again</p>
                         </div>
+
+                        <p className="text-3xl text-black text-left mt-10">About this Gig</p>
+
+                        <div className="h-full w-full mt-10">
+                            <div className="relative overflow-x-auto">
+
+                                <table class="h-full w-full">
+                                    <caption class="p-5 bg-cus-black-low rounded-tl-md rounded-tr-md">
+                                        <h2 className="text-3xl text-white text-left">Our products</h2>
+                                        <p className="text-sm text-white text-left mt-2">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
+                                    </caption>
+
+                                    <thead className="bg-gray-500">
+                                        <tr>
+                                            <th className="px-6 py-3 text-black text-lg font-semibold">
+                                                Package
+                                            </th>
+                                            <th className="px-6 py-3 text-black text-lg font-semibold">
+                                                Bronze
+                                            </th>
+                                            <th className="px-6 py-3 text-black text-lg font-semibold">
+                                                Silver
+                                            </th>
+                                            <th className="px-6 py-3 text-black text-lg font-semibold">
+                                                Gold
+                                            </th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr className="bg-cus-black-low border-b border-gray-500">
+                                            <th className="px-6 py-4 text-white text-base">
+                                                Apple MacBook Pro 17
+                                            </th>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                Silver
+                                            </td>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                Laptop
+                                            </td>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                $2999
+                                            </td>
+                                        </tr>
+                                        <tr className="bg-cus-black-low border-b border-gray-500">
+                                            <th className="px-6 py-4 text-white text-base">
+                                                Apple MacBook Pro 17
+                                            </th>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                Silver
+                                            </td>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                Laptop
+                                            </td>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                $2999
+                                            </td>
+                                        </tr>
+                                        <tr className="bg-cus-black-low border-b border-gray-500">
+                                            <th className="px-6 py-4 text-white text-base">
+                                                Apple MacBook Pro 17
+                                            </th>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                Silver
+                                            </td>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                Laptop
+                                            </td>
+                                            <td className="px-6 py-4 text-white text-base">
+                                                $2999
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="col-span-5 h-auto flex items-center justify-center pl-5">
+                    <div className="col-span-5 h-full flex items-center justify-center pl-5">
+
+                        <div class="border-b border-gray-200 dark:border-gray-700">
+                            <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-styled-tab" data-tabs-toggle="#default-styled-tab-content" data-tabs-active-classes="text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500" data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300" role="tablist">
+                                <li class="me-2" role="presentation">
+                                    <button class="inline-block p-4 border-b-2 rounded-t-lg" id="profile-styled-tab" data-tabs-target="#styled-profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+                                </li>
+                                <li class="me-2" role="presentation">
+                                    <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="dashboard-styled-tab" data-tabs-target="#styled-dashboard" type="button" role="tab" aria-controls="dashboard" aria-selected="false">Dashboard</button>
+                                </li>
+                                <li class="me-2" role="presentation">
+                                    <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-styled-tab" data-tabs-target="#styled-settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Settings</button>
+                                </li>
+                                <li role="presentation">
+                                    <button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="contacts-styled-tab" data-tabs-target="#styled-contacts" type="button" role="tab" aria-controls="contacts" aria-selected="false">Contacts</button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="default-styled-tab-content">
+                            <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                            </div>
+                            <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-dashboard" role="tabpanel" aria-labelledby="dashboard-tab">
+                                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                            </div>
+                            <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-settings" role="tabpanel" aria-labelledby="settings-tab">
+                                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                            </div>
+                            <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="styled-contacts" role="tabpanel" aria-labelledby="contacts-tab">
+                                <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
