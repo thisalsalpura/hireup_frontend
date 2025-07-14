cb994fbe015ffecbd250f8e5097596c8d32655e3
thisalsalpura <salpurathisal@gmail.com>
2025-07-14 23:02:49 +0530

    Update 6.1

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -77,12 +77,12 @@ const Login = () => {
                                         <div className="flex flex-row items-center justify-between mt-5">
                                             <Checkbox name="Remember Me" />
 
-                                            <label className="text-blue-700 hover:border-b-2 hover:border-blue-700 text-sm sm:text-base"><a href="/">Forgot Password?</a></label>
+                                            <label className="text-blue-700 border-b-2 border-transparent hover:border-blue-700 text-sm sm:text-base"><a href="/">Forgot Password?</a></label>
                                         </div>
 
                                         <Button href="/home" name="SignIn" containerClass="mt-8" frontClasses="text-white h-10 w-full border-2 border-white" backClasses="h-10 w-full bg-blue-700" />
 
-                                        <p className="text-white text-sm sm:text-base tracking-wide mt-4">Not registered? <a className="text-blue-700 hover:border-b-2 hover:border-blue-700" onClick={toggleSlide}>Register</a></p>
+                                        <p className="text-white text-sm sm:text-base tracking-wide mt-4">Not registered? <a className="text-blue-700 border-b-2 border-transparent hover:border-blue-700" onClick={toggleSlide}>Register</a></p>
 
                                         <Button onClick={() => { loginWithGoogle(); }} name="Google SignIn" icon={faGoogle} containerClass="mt-4" frontClasses="text-blue-700 h-10 w-full border-2 border-blue-700" backClasses="h-10 w-full bg-white" />
 
@@ -106,14 +106,20 @@ const Login = () => {
                                     <div className="h-full w-full flex flex-col bg-cus-black-low rounded-xl shadow-xl border border-black p-5 sm:p-8">
                                         <h2 className="text-white text-2xl text-left font-semibold">Register</h2>
 
-                                        <div className="flex flex-col w-full gap-1.5 mt-5">
-                                            <label htmlFor="fname" className="text-white text-sm">First Name</label>
-                                            <input id="fname" name="fname" className="bg-blur h-10 py-0.5 px-2.5 rounded-md text-white text-base" type="text" placeholder="John" required />
-                                        </div>
+                                        <div className="h-full w-full grid grid-cols-12 md:gap-4">
+                                            <div className="col-span-12 md:col-span-6">
+                                                <div className="flex flex-col w-full gap-1.5 mt-5">
+                                                    <label htmlFor="fname" className="text-white text-sm">First Name</label>
+                                                    <input id="fname" name="fname" className="bg-blur h-10 py-0.5 px-2.5 rounded-md text-white text-base" type="text" placeholder="John" required />
+                                                </div>
+                                            </div>
 
-                                        <div className="flex flex-col w-full gap-1.5 mt-5">
-                                            <label htmlFor="lname" className="text-white text-sm">Last Name</label>
-                                            <input id="lname" name="lname" className="bg-blur h-10 py-0.5 px-2.5 rounded-md text-white text-base" type="text" placeholder="Doily" required />
+                                            <div className="col-span-12 md:col-span-6">
+                                                <div className="flex flex-col w-full gap-1.5 mt-5">
+                                                    <label htmlFor="lname" className="text-white text-sm">Last Name</label>
+                                                    <input id="lname" name="lname" className="bg-blur h-10 py-0.5 px-2.5 rounded-md text-white text-base" type="text" placeholder="Doily" required />
+                                                </div>
+                                            </div>
                                         </div>
 
                                         <div className="flex flex-col w-full gap-1.5 mt-5">
@@ -121,9 +127,14 @@ const Login = () => {
                                             <input id="email" name="email" className="bg-blur h-10 py-0.5 px-2.5 rounded-md text-white text-base" type="email" placeholder="name@gmail.com" required />
                                         </div>
 
+                                        <div className="flex flex-col w-full gap-1.5 mt-5">
+                                            <label htmlFor="password" className="text-white text-sm">Password</label>
+                                            <input id="password" name="password" className="bg-blur h-10 py-0.5 px-2.5 rounded-md text-white text-base" type="password" placeholder="••••••••" required />
+                                        </div>
+
                                         <Button name="Register" containerClass="mt-8" frontClasses="text-white h-10 w-full border-2 border-white" backClasses="h-10 w-full bg-blue-700" />
 
-                                        <p className="text-white text-sm sm:text-base tracking-wide mt-4">Already registered? <a className="text-blue-700 hover:border-b-2 hover:border-blue-700" onClick={toggleSlide}>SignIn</a></p>
+                                        <p className="text-white text-sm sm:text-base tracking-wide mt-4">Already registered? <a className="text-blue-700 border-b-2 border-transparent hover:border-blue-700" onClick={toggleSlide}>SignIn</a></p>
                                     </div>
                                 </div>
 
